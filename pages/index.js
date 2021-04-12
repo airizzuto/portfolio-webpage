2e7a065f3360cbf2c5d8bcd467908306028b9650
airizzuto <agustinrizzuto@hotmail.com>
2021-04-12 17:17:25 -0300

    Add: head

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,12 @@ import Footer from "./components/Footer"
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Personal Porfolio</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="/static/favicon/favicon.ico"/>
+      </Head>
       <Header />
       <Main />
       <Footer />
