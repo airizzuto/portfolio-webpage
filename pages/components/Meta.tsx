dc29f5975b4aa32d67dea239ac23e1a37940a91d
airizzuto <agustinrizzuto@hotmail.com>
2021-04-13 12:45:59 -0300

    New: metadata

---
+++ pages/components/Meta.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head"
+
+export default function Meta() {
+    return (
+        <Head>
+            <title>Personal Porfolio</title>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <link rel="shortcut icon" href="/static/favicon/favicon.ico"/>
+        </Head>
+    )
+}
+
