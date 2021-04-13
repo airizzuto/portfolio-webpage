0433aefd6f4a967ae354f5a1af2a784824e89f69
airizzuto <agustinrizzuto@hotmail.com>
2021-04-13 13:19:25 -0300

    Fix: background
    Todo: Container style

--- pages/components/Content.tsx
+++ pages/components/Content.tsx
@@ -8,21 +8,16 @@ interface Props {
 
 export default function Content({title, text, imageSrc}: Props) {
     return (
-        <div className={styles.Content}>
-            <style jsx>
-                {`
-                    .Content {
-                        background-image: url(${imageSrc});
-                    }
-                `}
-            </style>
+        <div className={styles.Content} 
+            style={{
+                backgroundImage: `url(${imageSrc})`
+            }}>
             <h3 className={styles.Title}>{title}</h3>
             <div className={styles.Info}>
                 {text.map((paragraph) => {
                     return <p>{paragraph}</p>
                 })}
             </div>
-            
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
                     "Ducimus autem pariatur soluta sunt atque qui repellat. Dolores debitis illo vitae quas. Accusantium quaerat aspernatur odio aut. Magnam quis accusantium rerum at eum. Veritatis dolorum deleniti asperiores adipisci deleniti minus impedit ducimus.",
                     "Facere aliquid sunt maxime ad atque perferendis enim. Quam voluptas molestiae aut consequatur aut officiis. Est sit modi placeat quidem nemo et quod est. Praesentium enim et itaque quisquam minima numquam."]
                 }
-                imageSrc={"/static/christian-wiediger-WkfDrhxDMC8-unsplash.jpg"}/>
+                imageSrc={"static/christian-wiediger-WkfDrhxDMC8-unsplash.jpg"}/>
     </div>
   )
 }
