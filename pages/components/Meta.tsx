bd0a0b1273e4a83cba1edc694f2d921728318186
airizzuto <agustinrizzuto@hotmail.com>
2021-04-14 17:44:15 -0300

    Update: favicon

--- pages/components/Meta.tsx
+++ pages/components/Meta.tsx
@@ -3,7 +3,7 @@ import Head from "next/head"
 export default function Meta() {
     return (
         <Head>
-            <title>Personal Porfolio</title>
+            <title>PLACEHOLDER TITLE</title>
             <meta charSet="utf-8" />
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             <link rel="shortcut icon" href="/static/favicon/favicon.ico"/>
