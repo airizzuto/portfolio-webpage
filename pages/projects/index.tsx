1c5fe8411eb4666e9ba2334ce0d7d6c3ddf52ee5
airizzuto <agustinrizzuto@hotmail.com>
2021-04-14 18:02:19 -0300

    Fix: placeholder text corrected

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -3,7 +3,7 @@ import Content from "../components/Content"
 export default function Projects() {
     return (
         <div>
-            <Content title={"Contact"} text={["FORM PLACEHOLDER"]} imageSrc={"static/tianyi-ma.jpg"}/>
+            <Content title={"Projects"} text={["FORM PLACEHOLDER"]} imageSrc={"static/tianyi-ma.jpg"}/>
         </div>
     )
 }
