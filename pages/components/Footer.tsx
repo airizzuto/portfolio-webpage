1fdac6046eba3769f4d47cab5e23d6606544f0c6
airizzuto <agustinrizzuto@hotmail.com>
2021-04-16 14:39:22 -0300

    Add: icons

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -1,13 +1,16 @@
 import NavButton from "./NavButton"
 import styles from "../../styles/Footer.module.scss"
+import {FaGithub, FaLinkedin} from "react-icons/fa"
 
  const Footer = () => {
     return (
         <footer className={styles.Footer}>
-            <nav className={styles.FooterNav}>
-                <NavButton text={`Github`} href={"https://github.com/airizzuto"}/>
-                <NavButton text={"LinkedIn"} href={"/"}/>
-                <NavButton text={"Contact"} href={"/contact"}/>
+            <nav className={styles.Nav}>
+                <ul>
+                    <li><NavButton text={`Github`} href={"https://github.com/airizzuto"}/><FaGithub size={"2.5em"}/></li>
+                    <li><NavButton text={"LinkedIn"} href={"/"}/><FaLinkedin size={"2.5em"}/></li>
+                    <li><NavButton text={"Contact"} href={"/contact"}/></li>
+                </ul>
             </nav>
             <p className={styles.Copyright}>AIRIZZUTO © 2021</p>
         </footer>
