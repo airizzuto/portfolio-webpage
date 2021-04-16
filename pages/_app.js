fad6ab437f51f0ec70de7b21b0210ac0a6efd835
airizzuto <agustinrizzuto@hotmail.com>
2021-04-16 12:42:05 -0300

    Update: styles grid

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/globals.scss'
+import styles from '../styles/Layout.module.scss'
 import Layout from './components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
-import styles from '../styles/Layout.module.scss'
 import Content from "./components/Content"
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Content
                 title={"PLACEHOLDER TITLE"} 
                 text={"Welcome placeholder"}
                 imageSrc={"static/christian-wiediger-WkfDrhxDMC8-unsplash.jpg"}/>
-    </div>
+    </>
   )
 }
