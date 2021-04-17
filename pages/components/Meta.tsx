55b4e1b66858b752f2725485077a82c0a7b86c3a
airizzuto <agustinrizzuto@hotmail.com>
2021-04-17 13:26:13 -0300

    Add: CDN font

--- pages/components/Meta.tsx
+++ pages/components/Meta.tsx
@@ -8,6 +8,8 @@ export default function Meta() {
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             <meta name="keywords" content="programming, developing, web development" />
             <link rel="shortcut icon" href="/static/favicon/favicon.ico"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap" rel="stylesheet" /> 
         </Head>
     )
 }
