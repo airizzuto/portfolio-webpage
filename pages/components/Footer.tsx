7828da9f2c99c239b54d44fd2c7ba3a3effeaca5
airizzuto <agustinrizzuto@hotmail.com>
2021-04-17 13:29:38 -0300

    Fix: hover icon color

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -4,16 +4,27 @@ import {FaGithub, FaLinkedin} from "react-icons/fa"
 
  const Footer = () => {
     return (
-        <footer className={styles.Footer}>
-            <nav className={styles.Nav}>
-                <ul>
-                    <li><NavButton text={`Github`} href={"https://github.com/airizzuto"}/><FaGithub size={"2.5em"}/></li>
-                    <li><NavButton text={"LinkedIn"} href={"/"}/><FaLinkedin size={"2.5em"}/></li>
-                    <li><NavButton text={"Contact"} href={"/contact"}/></li>
-                </ul>
-            </nav>
-            <p className={styles.Copyright}>AIRIZZUTO © 2021</p>
-        </footer>
-    )
+      <footer className={styles.Footer}>
+        <nav className={styles.Nav}>
+          <ul>
+            <li>
+              <NavButton
+                text={`Github`}
+                href={"https://github.com/airizzuto"}
+              />
+              <FaGithub className={styles.Icon} size={"2.5em"} />
+            </li>
+            <li>
+              <NavButton text={"LinkedIn"} href={"/"} />
+              <FaLinkedin className={styles.Icon} size={"2.5em"} />
+            </li>
+            <li>
+              <NavButton text={"Contact"} href={"/contact"} />
+            </li>
+          </ul>
+        </nav>
+        <p className={styles.Copyright}>AIRIZZUTO © 2021</p>
+      </footer>
+    );
 }
 export default Footer
