3895d7bf2032892eb09a714c14f4e9718f44474f
airizzuto <agustinrizzuto@hotmail.com>
2021-04-22 12:03:03 -0300

    Fix: logo

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
         <header className={styles.Header}>
             <Link href="/">
                 <a className={styles.HeaderLogo}>
-                    <Image src={"/static/logo.svg"} alt="logo" width="70em" height="80px" />
+                    <Image src={"/static/proto-logo-dark4.svg"} alt="logo" width="70em" height="80px" />
                 </a>
             </Link>
             
