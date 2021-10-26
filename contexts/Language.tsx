2c91325e615eb1e1cde6d377a5bcc877a5c51b3a
airizzuto <agustinrizzuto@hotmail.com>
2021-10-26 12:40:10 -0300

    Added: language selector scaffolded

---
+++ contexts/Language.tsx
@@ -0,0 +1,30 @@
+import { createContext, useState } from 'react';
+import { languageOptions, dictionaryList } from '../languages/languages';
+
+// create the language context with default selected language
+export const LanguageContext = createContext({
+  userLanguage: 'en',
+  dictionary: dictionaryList.en
+});
+
+// it provides the language context to app
+export function LanguageProvider({ children }) {
+  const defaultLanguage = window.localStorage.getItem('rcml-lang');
+  const [userLanguage, setUserLanguage] = useState(defaultLanguage || 'en');
+
+  const provider = {
+    userLanguage,
+    dictionary: dictionaryList[userLanguage],
+    userLanguageChange: selected => {
+      const newLanguage = languageOptions[selected] ? selected : 'en'
+      setUserLanguage(newLanguage);
+      window.localStorage.setItem('rcml-lang', newLanguage);
+    }
+  };
+
+  return (
+    <LanguageContext.Provider value={provider}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
