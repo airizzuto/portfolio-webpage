72268e473240aa14c1803880d2f355f95a6886ff
airizzuto <agustinrizzuto@hotmail.com>
2021-10-27 12:52:45 -0300

    Fixed: cors declaration

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -8,7 +8,7 @@ export default function Meta() {
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             <meta name="keywords" content="programming, developing, web development, full stack, software" />
             <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
             <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" /> 
             <link rel="shortcut icon" href="/static/favicon/favicon.ico"/>
         </Head>
