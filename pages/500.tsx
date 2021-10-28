f907337bc9ba97540ceecd5df8c69470647a5203
airizzuto <agustinrizzuto@hotmail.com>
2021-10-28 12:34:58 -0300

    Added: custom placeholder error pages

---
+++ pages/500.tsx
@@ -0,0 +1,8 @@
+import Style from "../styles/ErrorPage.module.scss";
+
+// TODO:
+export default function Custom500() {
+  return (
+    <h1 className={Style.Container}>500 - Server-side error occurred</h1>
+  );
+}
