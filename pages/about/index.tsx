bad37a3d253ce15e40a77e633b2a9152b6317321
airizzuto <agustinrizzuto@hotmail.com>
2021-11-01 12:58:13 -0300

    Updated: about page content and translation

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,30 +1,36 @@
 
+import useTranslation from "next-translate/useTranslation";
+
 import Style from "../../styles/ContentDisplay.module.scss";
 
 export default function About() {
-    return (
-      <div className={Style.Container}
-      style={{
-        backgroundImage: `url(${"static/nihon-graphy.jpg"})`
-      }}
-    >
-      <div className={Style.Background}>
-        <h1 className={Style.Title}>About Me</h1>
-        <div className={Style.Text}>
-          <p>
-            I am Full Stack software developer using .NET, PostgreSQL, React and Typescript.
-          </p>
-          <p>
-            Before I used to work as a CAD designer and analyst of CFD/FEM thermal/structural simulations in the aerospace industry.
-          </p>
-          <p>
-            Programming always had my interest and started to study it to further improve my productivity and quality of work. Focused on scripting and data analysis at first, and switching to development after realizing the potential it has for creating the future technologies and the challenges that it provided.
-          </p>
-          <p>
-            I am also a pilot of airplane, and coursed the commercial pilot training.
-          </p>
-        </div>
+  const { t, lang } = useTranslation('about');
+  const title = t("content_title");
+  const text = t("content_text", {}, {returnObjects: true} );
+
+  return (
+    <div className={Style.Container}
+    style={{
+      backgroundImage: `url(${"static/nihon-graphy.jpg"})`
+    }}
+  >
+    <div className={Style.Background}>
+      <h1 className={Style.Title}>{title}</h1>
+      <div className={Style.Text}>
+        <p>
+          {text[0]}
+        </p>
+        <p>
+          {text[1]}
+        </p>
+        <p>
+          {text[2]}
+        </p>
+        <p>
+          {text[3]}
+        </p>
       </div>
     </div>
-    )
+  </div>
+  )
 }
