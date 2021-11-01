e14790267d7ad363d2bdf487791cf100c9201ba0
airizzuto <agustinrizzuto@hotmail.com>
2021-11-01 17:10:43 -0300

    Updated: air ranging translations

--- pages/projects/AirRangingProject.tsx
+++ pages/projects/AirRangingProject.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import useTranslation from 'next-translate/useTranslation';
+
 import ContentWrapper from '../../components/Content/ContentWrapper';
 import ContentLink from '../../components/Content/ContentLinks';
 
 import Style from "../../styles/ContentDisplay.module.scss";
 
 const AirRangingProject = () => {
+  const { t, lang } = useTranslation('projects:air-ranging');
+  const title = t("title");
+  const content: string[] = t("content", {}, {returnObjects: true} );
+  const webLabel = t("links.web");
+
+  // TODO: insert translation
   return (
     <ContentWrapper image={'static/AirRangingMap.png'}>
       <h1 className={Style.Title}>
