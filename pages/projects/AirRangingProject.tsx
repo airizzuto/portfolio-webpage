f26412ad8a2dc218ffc1d938a82789ab6107f988
airizzuto <agustinrizzuto@hotmail.com>
2021-11-02 10:58:09 -0300

    Updated: projects translations

--- pages/projects/AirRangingProject.tsx
+++ pages/projects/AirRangingProject.tsx
@@ -7,27 +7,23 @@ import ContentLink from '../../components/Content/ContentLinks';
 import Style from "../../styles/ContentDisplay.module.scss";
 
 const AirRangingProject = () => {
-  const { t, lang } = useTranslation('projects:air-ranging');
-  const title = t("title");
-  const content: string[] = t("content", {}, {returnObjects: true} );
+  const { t, lang } = useTranslation('projects');
+  const title = t("air-ranging.title");
+  const content: string[] = t("air-ranging.content", {}, {returnObjects: true} );
   const webLabel = t("links.web");
 
-  // TODO: insert translation
   return (
     <ContentWrapper image={'static/AirRangingMap.png'}>
-      <h1 className={Style.Title}>
-        Air Ranging Web Application
-      </h1>
+      <h1 className={Style.Title}>{title}</h1>
+
       <div className={Style.Text}>
-        <p>Web application for flight fuel planning, with aircraft management and user account.</p>
-        <p>WebAPI created using .NET and Entity Framework.</p>
-        <p>Database using PostgreSQL.</p>
-        <p>Frontend developed with React, Typescript and Sass.</p>
+        {content.map((text, idx) => {
+          return <p key={`porfolio-text-${idx}`}>{text}</p>
+        })}
       </div>
 
-      {/* TODO: abstract */}
       <div className={Style.Links}>
-        <ContentLink label={"Web Page"} href={"https://airizzutodeveloper.vercel.app/"} />
+        <ContentLink label={"Web"} href={"https://airizzutodeveloper.vercel.app/"} />
         <ContentLink label={"Github"} href={"https://github.com/airizzuto/AirRanging/"} />
       </div>
     </ContentWrapper>
--- pages/projects/PortfolioProject.tsx
+++ pages/projects/PortfolioProject.tsx
@@ -7,20 +7,21 @@ import ContentLink from '../../components/Content/ContentLinks';
 import Style from "../../styles/ContentDisplay.module.scss";
 
 const PortfolioProject = () => {
-  const { t, lang } = useTranslation('projects:portfolio');
-  const title = t("title");
-  const content: string[] = t("content", {}, {returnObjects: true} );
+  const { t, lang } = useTranslation('projects');
+  const title = t("portfolio.title");
+  const content: string[] = t("portfolio.content", {}, {returnObjects: true} );
   const webLabel = t("links.web");
 
   return (
     <ContentWrapper image={"static/tianyi-ma.jpg"}>
-      <h1 className={Style.Title}>
-        Personal Portfolio Webpage
-      </h1>
+      <h1 className={Style.Title}>{title}</h1>
+
       <div className={Style.Text}>
-        <p>Small portfolio website made to showcase personal projects and information about myself.</p>
-        <p>Tools used for FE: Next, React, Typescript and Sass</p>
+        {content.map((text, idx) => {
+          return <p key={`porfolio-text-${idx}`}>{text}</p>
+        })}
       </div>
+
       <div className={Style.Links}>
         <ContentLink 
           label={"Github"}
