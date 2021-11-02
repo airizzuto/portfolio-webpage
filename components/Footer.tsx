cfd7e820bff08c6fa8fa54636913a4bdb0a6a0e8
airizzuto <agustinrizzuto@hotmail.com>
2021-11-02 11:23:48 -0300

    Updated: footer i18n

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,15 @@
 import NavButton from "./Header/NavButton"
+import useTranslation from "next-translate/useTranslation";
+
 import {FaGithub, FaLinkedin} from "react-icons/fa"
 
 import styles from "../styles/Footer.module.scss"
 
  const Footer = () => {
+    const { t, lang } = useTranslation('footer');
+    const githubLink = t("github.link");
+    const contactLabel = t("contact.label");
+
     return (
       <footer className={styles.Footer}>
         <nav className={styles.Nav}>
@@ -16,11 +22,11 @@ import styles from "../styles/Footer.module.scss"
               <FaGithub className={styles.Icon} size={"1rem"} />
             </li>
             <li>
-              <NavButton text={"LinkedIn"} href={"https://www.linkedin.com/in/airizzuto/"} />
+              <NavButton text={"LinkedIn"} href={githubLink} />
               <FaLinkedin className={styles.Icon} size={"1rem"} />
             </li>
             <li>
-              <NavButton text={"Contact"} href={"/contact"} />
+              <NavButton text={contactLabel} href={"/contact"} />
             </li>
           </ul>
         </nav>
