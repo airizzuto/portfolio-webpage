c4d8ca2c498c3a76bdfd2c7351d5405c1cf6864d
airizzuto <agustinrizzuto@hotmail.com>
2021-11-08 10:46:56 -0300

    Updated: projects to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,16 @@
-import useTranslation from "next-translate/useTranslation";
+import React from "react";
 
-import ContentWrapper from "../components/Content/ContentWrapper";
-
-import Style from "../styles/ContentDisplay.module.scss";
+import Landing from "./Landing";
+import AirRangingProject from "./projects/AirRangingProject";
+import PortfolioProject from "./projects/PortfolioProject";
 
 export default function Home() {
-  const { t, lang } = useTranslation('home');
-  const title = t("content_title");
-  const subtitle = t("content_subtitle");
-  const content = [...t("content_text", {}, {returnObjects: true})];
 
   return (
-    <ContentWrapper image={"static/christian-wiediger-WkfDrhxDMC8-unsplash.jpg"}>
-      <h1 className={Style.Title}>{title}</h1>
-      <h2 className={Style.SubTitle}>{subtitle}</h2>
-      <div className={Style.Text}>
-        {content.map((text, idx) => {
-          return <p key={`home--text-${idx}`}>{text}</p>
-        })}
-      </div>
-    </ContentWrapper>
+    <>
+      <Landing />
+      <AirRangingProject />
+      <PortfolioProject />
+    </>
   )
 }
