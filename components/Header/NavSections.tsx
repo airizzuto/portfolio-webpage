d843d8a356c5148501d3f9f2299c6943edea4f16
airizzuto <agustinrizzuto@hotmail.com>
2021-11-08 10:47:25 -0300

    Updated: projects to home

--- components/Header/NavSections.tsx
+++ components/Header/NavSections.tsx
@@ -11,14 +11,12 @@ const NavSections: React.FC<Props> = ({handleClick}) => {
   
   const { t, lang } = useTranslation('common');
   const home = t("nav_home");
-  const projects = t("nav_projects");
   const about = t("nav_about");
   const contact = t("nav_contact");
 
   return (
     <ul>
       <NavButton text={home} href={"/"} handleClick={handleClick}/>
-      <NavButton text={projects} href={"/projects"} handleClick={handleClick}/>
       <NavButton text={about} href={"/about"} handleClick={handleClick}/>
       <NavButton text={contact} href={"/contact"} handleClick={handleClick}/>
     </ul>
---
+++ pages/Landing.tsx
@@ -0,0 +1,25 @@
+import useTranslation from "next-translate/useTranslation";
+import React from "react";
+
+import ContentWrapper from "../components/Content/ContentWrapper";
+
+import Style from "../styles/ContentDisplay.module.scss";
+
+export default function Landing() {
+  const { t, lang } = useTranslation('home');
+  const title = t("content_title");
+  const subtitle = t("content_subtitle");
+  const content = [...t("content_text", {}, {returnObjects: true})];
+
+  return (
+    <ContentWrapper image={"static/christian-wiediger-WkfDrhxDMC8-unsplash.jpg"}>
+      <h1 className={Style.Title}>{title}</h1>
+      <h2 className={Style.SubTitle}>{subtitle}</h2>
+      <div className={Style.Text}>
+        {content.map((text, idx) => {
+          return <p key={`home--text-${idx}`}>{text}</p>
+        })}
+      </div>
+    </ContentWrapper>
+  )
+}
