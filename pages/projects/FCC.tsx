fd89eceee7ac61b5f35c3ef53f36e884d47ae47e
airizzuto <agustinrizzuto@hotmail.com>
2021-11-11 12:18:45 -0300

    Updated: mosaic layout

--- pages/projects/FCC.tsx
+++ pages/projects/FCC.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import useTranslation from "next-translate/useTranslation";
 
 import ContentWrapper from '../../components/Content/ContentWrapper';
+import ContentCard from '../../components/Content/ContentCard';
+import FCCProject from './FCCProject';
 
 import Style from "../../styles/ContentDisplay.module.scss";
-import FCCProject from './FCCProject';
-import ContentDetailed from '../../components/Content/ContentDetailed';
 
 const FCC = () => {
   const { t, lang } = useTranslation('projects');
@@ -13,27 +13,25 @@ const FCC = () => {
 
   return (
     <ContentWrapper image={"static/christian-wiediger-WkfDrhxDMC8-unsplash.jpg"}>
-      <ContentDetailed>
-        <h1 className={Style.Title}>{title}</h1>
-        <div className={Style.Mosaic}>
-          <FCCProject
-            imagePath={'static/random-quote-generator.png'}
-            projectName={'random-quote-generator'}
-          />
-          <FCCProject 
-            imagePath={'static/drum-machine.png'}
-            projectName={'drum-machine'}
-          />
-          <FCCProject 
-            imagePath={'static/markdown-previewer.png'}
-            projectName={'markdown-previewer'}
-          />
-          <FCCProject 
-            imagePath={'static/calculator.png'}
-            projectName={'calculator'}
-          />
-        </div>
-      </ContentDetailed>
+      <h1 className={Style.Title}>{title}</h1>
+      <ContentCard>
+        <FCCProject
+          imagePath={'static/random-quote-generator.png'}
+          projectName={'random-quote-generator'}
+        />
+        <FCCProject 
+          imagePath={'static/drum-machine.png'}
+          projectName={'drum-machine'}
+        />
+        <FCCProject 
+          imagePath={'static/markdown-previewer.png'}
+          projectName={'markdown-previewer'}
+        />
+        <FCCProject 
+          imagePath={'static/calculator.png'}
+          projectName={'calculator'}
+        />
+      </ContentCard>
     </ContentWrapper>
   )
 }
