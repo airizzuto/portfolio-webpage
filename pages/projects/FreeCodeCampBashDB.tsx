9410fabef45a75497396d48ea9eb9a189ce90842
Agustín I. Rizzuto <60024435+airizzuto@users.noreply.github.com>
2022-08-02 12:53:27 -0300

    update: page course to table component

--- pages/projects/FreeCodeCampBashDB.tsx
+++ pages/projects/FreeCodeCampBashDB.tsx
@@ -2,12 +2,11 @@ import React from 'react'
 import useTranslation from "next-translate/useTranslation";
 
 import ContentWrapper from '../../components/section/ContentWrapper';
-import ContentMosaic from '../../components/section/ContentMosaic';
 import ContentPage from '../../components/section/ContentPage';
-import CardWrapper from '../../components/card/CardWrapper';
-import CardProject from '../../components/card/CardProject';
 
 import Content from "../../styles/content/ContentPage.module.scss";
+import ContentList from '../../components/section/ContentList';
+import ListItem from '../../components/list/ListItem';
 
 const FreeCodeCampBashDB = () => {
   const { t, lang } = useTranslation('projects');
@@ -23,24 +22,38 @@ const FreeCodeCampBashDB = () => {
           }}
         >{title}</h1>
 
-        {/* TODO: Make list-table with project name | technologies applied | github repo */}
-        <ContentMosaic>
-          <CardWrapper imagePath={''}>
-            <CardProject projectName={"Celestial Bodies DB"} label={"Github"}/>
-          </CardWrapper>
-          <CardWrapper imagePath={''}>
-            <CardProject projectName={"World Cup DB"} label={"Github"}/>
-          </CardWrapper>
-          <CardWrapper imagePath={''}>
-            <CardProject projectName={"Salon Appointment Scheduler Bash/DB"} label={"Github"}/>
-          </CardWrapper>
-          <CardWrapper imagePath={''}>
-            <CardProject projectName={"Periodic Table Bash/DB"} label={"Github"}/>
-          </CardWrapper>
-          <CardWrapper imagePath={''}>
-            <CardProject projectName={"Number Guesser Bash/DB"} label={"Github"}/>
-          </CardWrapper>
-        </ContentMosaic>
+        <ContentList>
+          <ListItem
+            projectName={'Celestial Bodies DB'}
+            technologies={["PostgreSQL"]}
+            website={'Github'}
+            link={'https://github.com/airizzuto/FCC-CelestialBodiesDatabase'}
+          />
+          <ListItem
+            projectName={'World Cup DB'}
+            technologies={["PostgreSQL"]}
+            website={'Github'}
+            link={'https://github.com/airizzuto/FCC-WorldCupDatabase'}
+          />
+          <ListItem
+            projectName={'Salon Appointment Scheduler'}
+            technologies={["Bash", "PostgreSQL"]}
+            website={'Github'}
+            link={'https://github.com/airizzuto/FCC-SalonAppointmentScheduler'}
+          />
+          <ListItem
+            projectName={'Periodic Table'}
+            technologies={["Bash", "PostgreSQL"]}
+            website={'Github'}
+            link={'https://github.com/airizzuto/FCC-PeriodicTable'}
+          />
+          <ListItem
+            projectName={'Number Guesser'}
+            technologies={["Bash", "PostgreSQL"]}
+            website={'Github'}
+            link={'https://github.com/airizzuto/FCC-NumberGuesser'}
+          />
+        </ContentList>
       </ContentPage>
     </ContentWrapper>
   )
